bbcfc12ea340b8b919aad266a612d8f1a918f0bd
Shr4528 <Shr4528@gmail.com>
2023-07-05 17:10:40 -0500

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+  title: string;
+};
+
+const Header = (props: Props) => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Header;
