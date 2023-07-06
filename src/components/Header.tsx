8d36cc2a0129db1f65a5cbb8b1f72903aa0d2157
Shr4528 <Shr4528@gmail.com>
2023-07-05 20:45:47 -0500

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,80 @@
-import React from 'react';
+import App from 'App.css';
+import { FaLinkedin, FaYoutube } from 'react-icons/fa';
+//import Nemera from '../assets/nemera.gif';
+import images from '../assets/images.png';
+import nem from '../assets/nem.png';
 
-type Props = {
-  title: string;
-};
+type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <nav className='navbar  navbar-expand-lg mr-40 text-bg-warning p-3'>
+      {/* <img className='image' src={nem} alt='' /> */}
+      <a className='navbar-brand' href='#'>
+        Nemera
+      </a>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarNav'
+        aria-controls='navbarNav'
+        aria-expanded='false'
+        aria-label='Toggle navigation'>
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarNav'>
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <a className='nav-link' href='#who'>
+              Who Are We
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#products'>
+              Our Products
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#services'>
+              Our Services
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#capabilities'>
+              Our Capabilities
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#careers'>
+              Careers
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#news'>
+              News
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#contact'>
+              Contact
+            </a>
+          </li>
+        </ul>
+        <ul className='navbar-nav ml-auto social-list__icon'>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>
+              <FaLinkedin style={{ color: '#0e76a8' }} />
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>
+              <FaYoutube style={{ color: '#ff0000' }} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
